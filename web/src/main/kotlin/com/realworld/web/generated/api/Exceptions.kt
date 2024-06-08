b29ca9be09ea9a46cde2ae7d30e3b3216bc78f62
Gene Nakagaki <gene.nakagaki@gmail.com>
2024-06-08 20:09:51 +0900

    fix: resolve build errors from OpenApi generated code

--- web/src/main/kotlin/com/realworld/web/generated/api/Exceptions.kt
+++ web/src/main/kotlin/com/realworld/web/generated/api/Exceptions.kt
@@ -1,29 +1,33 @@
 package com.realworld.web.generated.api
 
+import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import jakarta.servlet.http.HttpServletResponse
-import jakarta.validation.ConstraintViolationException
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
 
 // TODO Extend ApiException for custom exception handling, e.g. the below NotFound exception
-sealed class ApiException(msg: String, val code: Int) : Exception(msg)
+sealed class ApiException(msg: String, val code: HttpStatus) : Exception(msg)
 
-class NotFoundException(msg: String, code: Int = HttpStatus.NOT_FOUND.value()) : ApiException(msg, code)
+class NotFoundException(msg: String, code: HttpStatus = HttpStatus.NOT_FOUND) : ApiException(msg, code)
 
 
 @ControllerAdvice
 class DefaultExceptionHandler {
 
     @ExceptionHandler(value = [ApiException::class])
-    fun onApiException(ex: ApiException, response: HttpServletResponse): Unit =
-        response.sendError(ex.code, ex.message)
+    fun onApiException(ex: ApiException): Mono<ServerResponse> =
+        ServerResponse.status(ex.code)
+            .bodyValue(ex.message!!)
 
     @ExceptionHandler(value = [NotImplementedError::class])
-    fun onNotImplemented(ex: NotImplementedError, response: HttpServletResponse): Unit =
-        response.sendError(HttpStatus.NOT_IMPLEMENTED.value())
+    fun onNotImplemented(ex: NotImplementedError): Mono<ServerResponse> =
+        ServerResponse.status(HttpStatus.NOT_IMPLEMENTED)
+            .bodyValue(ex.message!!)
 
     @ExceptionHandler(value = [ConstraintViolationException::class])
-    fun onConstraintViolation(ex: ConstraintViolationException, response: HttpServletResponse): Unit =
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.constraintViolations.joinToString(", ") { it.message })
+    fun onConstraintViolation(ex: ConstraintViolationException): Mono<ServerResponse> =
+        ServerResponse.status(HttpStatus.BAD_REQUEST)
+            .bodyValue(ex.constraintViolations.joinToString(", ") {it.message})
 }
